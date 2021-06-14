7c3a4aad938edf15871eb5bfe40c206c01170b31
Ecline Koech <eclinejerutokoech@gmail.com>
2021-06-14 04:56:18 +0300

    modified js

--- script.js
+++ script.js
@@ -2,5 +2,26 @@
 // get month of birt from form
 // get day of birth from form
 // get akan name
+function dayName(birthday) {
+    if (month <= 12) { proceed);
+        
+    } else (month >12) {
+        reject submission
+    }
+}
 
-var daysOfWeek
+var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',];
+var maleNames = ["Kwasi", "Kwadwo",  "Kwabena", "Kwaku", "Yaw", "Kofi",  "Kwame",];
+var femaleNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama",];
+
+
+
+// var malenames = prompt("enter your date of birth");
+
+// var myModal = document.getElementById('myModal')
+
+// myModal.addEventListener('show.bs.modal', function (event) {
+//   if (!data) {
+//     return event.preventDefault() // stops modal from being shown
+//   }
+// })
