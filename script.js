2c53b45bf31a06e6ca0a7591cd73f4f37e62e0ed
Ecline Koech <eclinejerutokoech@gmail.com>
2021-06-15 16:30:37 +0300

    update js

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 // get year of birth from form
-// get month of birt from form
-// get day of birth from form
+
 // get akan name
-// function dayName(birthday) {
-//     if (month <= 12) { proceed);
-        
-//     } else (month >12) {
-//         reject submission
-//     }
-// }
+function dayName() {
+   var birthYear = parseInt(document.getElementById("birthYear").value);
 
-// var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',];
-// var maleNames = ["Kwasi", "Kwadwo",  "Kwabena", "Kwaku", "Yaw", "Kofi",  "Kwame",];
-// var femaleNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama",];
+   
+    // get month of birt from form
+    var month = parseInt(document.getElementById("month").value);
+    
+    if(month >12 || month <= 0 ) {
+        alert("Enter a valid month");
+    }
+    // get day of birth from form
+    var day = parseInt(document.getElementById("date").value);
+
+    if(day <=0 || day > 31) {
+        alert("Enter a valid date");
+    }
+    
+    var dayNumber = new Date (birthYear,month-1,day).getDay();
+    console.log(dayNumber);
+
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi",  "Kwame"];
+    var femaleNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var male = document.getElementById("flexRadioDefault1");
+    var female = document.getElementById("flexRadioDefault2")
+
+    if(male.checked) {
+        alert("Your Akan Name is: " + maleNames[dayNumber]);
+    } else if(female.checked){
+        alert("Your Akan Name is: " + femaleNames[dayNumber])
+    } else {
+        alert("Select your gender");
+    }
+    
+
+}
 
 
 
-// var malenames = prompt("enter your date of birth");
-
-// var myModal = document.getElementById('myModal')
-
-// myModal.addEventListener('show.bs.modal', function (event) {
-//   if (!data) {
-//     return event.preventDefault() // stops modal from being shown
-//   }
-// })
